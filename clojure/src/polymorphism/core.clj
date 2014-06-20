e529a7f6ba26657b917bb23243b9fc88639c21a1
nathanmesserbbc <nathan.messer@bbc.co.uk>
2014-06-20 15:23:23 +0100

    Multimethods

--- clojure/src/polymorphism/core.clj
+++ clojure/src/polymorphism/core.clj
@@ -1,16 +1,36 @@
-(ns polymorphism.core)
+(ns polymorphism.core
+  (:require [polymorphism.decode :refer :all]
+            [polymorphism.licence :refer :all]))
 
-(defn decode-audio
-  "Decode Audio from a media object"
-  [media]
-  "")
+(defmulti decode-audio :type)
+(defmulti decode-video :type)
 
-(defn decode-video
-  "Decode Video from a media object"
-  [media]
-  "")
+(defmethod decode-audio :simple [media]
+  (simple-audio media))
+(defmethod decode-video :simple [media]
+  (simple-video media))
+
+(defmethod decode-audio :advanced-video [media]
+  (simple-audio media))
+(defmethod decode-video :advanced-video [media]
+  (adv-video media))
+
+(defmethod decode-audio :advanced-audio [media]
+  (adv-audio media))
+(defmethod decode-video :advanced-audio [media]
+  (simple-video media))
+
+(defmethod decode-audio :advanced-both [media]
+  (adv-audio media))
+(defmethod decode-video :advanced-both [media]
+  (adv-video media))
 
 (defn create
   "Create a media object"
   [type audio video]
-  {})
+  {
+    :type type,
+    :audio audio,
+    :video video
+   })
+
