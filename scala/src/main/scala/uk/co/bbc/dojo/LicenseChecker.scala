c9b2a2d8775a587cf4ec69fb0c2f4fd67b9e520e
lalkhum <lalkhum.sanga@bbc.co.uk>
2014-06-30 12:30:54 +0100

    Update LicenseChecker.scala

--- scala/src/main/scala/uk/co/bbc/dojo/LicenseChecker.scala
+++ scala/src/main/scala/uk/co/bbc/dojo/LicenseChecker.scala
@@ -1,5 +1,12 @@
 package uk.co.bbc.dojo
 
 class LicenseChecker {
-  def needsLicense(toCheck: Media): Boolean = ???
+  def needsLicense(toCheck: Media): Boolean = {
+
+    toCheck.mediaType match {
+      case "simple" if toCheck.video.length < 5 => false
+      case _ => true
+    }
+  }
+
 }
